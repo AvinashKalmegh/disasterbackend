174fe0c9262c186661b76d3302adec4d665be9d8
AvinashKalmegh <avinashkalmegh93@gmail.com>
2024-01-17 22:08:48 +0530

    add

--- Models/entry.model.js
+++ Models/entry.model.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const entrySchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
     email: {type: String,unique:true},
+    mobile: Number,
+    countryCode: Number,
+    link:String,
     access: Boolean
 })
 
